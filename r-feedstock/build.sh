75d1363d7695e86d9d37ed1931be2cb945acba77
Ray Donnelly <mingw.android@gmail.com>
2017-11-29 04:25:33 -0800

    Preliminary macOS work

--- r-feedstock/build.sh
+++ r-feedstock/build.sh
@@ -3,9 +3,19 @@
 # If conda-build used libarchive to unpack things this would not need to exist
 mkdir -p unpack
 pushd unpack
-  if [[ -f ../SRO_3.4.1.0_1033.cab ]]; then
+  if [[ -f ../SRO_${PKG_VERSION}.0_1033.cab ]]; then
     python -c 'import libarchive, os; libarchive.extract_file("../SRO_3.4.1.0_1033.cab")'
-  elif [[ -f ../microsoft-r-open-3.4.1.pkg ]]; then
-    python -c 'import libarchive, os; libarchive.extract_file("../microsoft-r-open-3.4.1.pkg")'
+  elif [[ -f ../microsoft-r-open-${PKG_VERSION}.pkg ]]; then
+    if [[ $(uname) == Darwin ]]; then
+      xar -xf ../microsoft-r-open-${PKG_VERSION}.pkg
+      for PAYLOAD in $(find . -name Payload); do
+        cat "${PAYLOAD}" | gunzip -dc | cpio -i
+      done
+      rm -rf R_Open_App.pkg R_Open_Framework.pkg Distribution
+    else
+      # libarchive cannot handle this file unfortunately, not even on macOS itself.
+      python -c 'import libarchive, os; libarchive.extract_file("../microsoft-r-open-3.4.1.pkg")'
+    fi
   fi
+  find . | sort > ${RECIPE_DIR}/filelist-mro-${target_platform}.txt
 popd
--- r-feedstock/install-r-matrix.sh
+++ r-feedstock/install-r-matrix.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/library/
-mv unpack/library/Matrix ${PREFIX}/library/
+if [[ ${target_platform} == osx-64 ]]; then
+  FRAMEWORK=/Library/Frameworks/R.framework
+  LIBRARY=${FRAMEWORK}/Versions/${PKG_VERSION}-MRO/Resources/library
+else
+  FRAMEWORK=
+  LIBRARY=${FRAMEWORK}/library
+fi
+
+mkdir -p "${PREFIX}"${LIBRARY}
+mv unpack"${LIBRARY}"/Matrix "${PREFIX}"${LIBRARY}/
