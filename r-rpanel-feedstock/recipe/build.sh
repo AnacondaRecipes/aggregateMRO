e0e69bb0fc9e84b2cd8bab1331dbcfadd8f352e6
Ray Donnelly <mingw.android@gmail.com>
2018-01-29 21:08:16 +0000

    r-rpanel: Fix typo

--- r-rpanel-feedstock/recipe/build.sh
+++ r-rpanel-feedstock/recipe/build.sh
@@ -4,7 +4,7 @@ if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]]; then
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
   grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-  if [[ $target_paltform =~ linux.* ]]; then
+  if [[ $target_platform =~ linux.* ]]; then
     xvfb-run $R CMD INSTALL --build .
   else
     $R CMD INSTALL --build .
