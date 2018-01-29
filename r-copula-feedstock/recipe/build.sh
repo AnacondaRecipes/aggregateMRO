e7378ba32ce5c4ff7bc2f90a7ecf04bb71a8a7a6
Ray Donnelly <mingw.android@gmail.com>
2018-01-29 11:42:36 +0000

    Add the other R packages we build

---
+++ r-copula-feedstock/recipe/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]]; then
+  export DISABLE_AUTOBREW=1
+  mv DESCRIPTION DESCRIPTION.old
+  grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+  $R CMD INSTALL --build .
+else
+  mkdir -p $PREFIX/lib/R/library/copula
+  mv * $PREFIX/lib/R/library/copula
+fi
