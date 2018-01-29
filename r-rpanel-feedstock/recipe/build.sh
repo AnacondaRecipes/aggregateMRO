60d9a3d0c45841060ad137ac1aad5e62370babfd
Ray Donnelly <mingw.android@gmail.com>
2018-01-29 20:56:31 +0000

    r-rpanel: Also build via xfvb-run

--- r-rpanel-feedstock/recipe/build.sh
+++ r-rpanel-feedstock/recipe/build.sh
@@ -4,7 +4,11 @@ if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]]; then
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
   grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-  $R CMD INSTALL --build .
+  if [[ $target_paltform =~ linux.* ]]; then
+    xvfb-run $R CMD INSTALL --build .
+  else
+    $R CMD INSTALL --build .
+  fi
 else
   mkdir -p $PREFIX/lib/R/library/rpanel
   mv * $PREFIX/lib/R/library/rpanel
