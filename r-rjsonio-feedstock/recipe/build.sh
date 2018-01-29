d334ccb801bae5c0f329fa159ce1f74f3dec20b7
Ray Donnelly <mingw.android@gmail.com>
2018-01-29 21:44:33 +0000

    r-rjsonio: Filter out --std= from CXXFLAGS

--- r-rjsonio-feedstock/recipe/build.sh
+++ r-rjsonio-feedstock/recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]]; then
+  # Filter out -std=.* from CXXFLAGS as it disrupts checks for C++ language levels.
+  re='(.*[[:space:]])\-std\=[^[:space:]]*(.*)'
+  if [[ "${CXXFLAGS}" =~ $re ]]; then
+    export CXXFLAGS="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+  fi
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
   grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
