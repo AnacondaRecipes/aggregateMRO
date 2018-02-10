34d4ef33c2c1a18a913859087f78daf612c929f0
Ray Donnelly <mingw.android@gmail.com>
2018-02-10 18:55:13 +0000

    Some filelists

---
+++ generate-filelists.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+VER=3.4.3
+mkdir -p ~/conda/aggregateMRO/filelists-${VER}
+pushd ~/conda/aggregateMRO/filelists-${VER}
+  for PLATFORM in win-32 win-64 linux-32 linux-64 osx-64; do
+    python ~/conda/private_conda_recipes/rays-scratch-scripts/binstar_copy.py --owner rdonnellyr --platform $PLATFORM --operation list > rdonnellyr-${PLATFORM}.txt
+  done
+  for PLATFORM in win-64 linux-64; do
+    python ~/conda/private_conda_recipes/rays-scratch-scripts/binstar_copy.py --owner mro_test --platform $PLATFORM --operation list > mro_test-${PLATFORM}.txt
+  done
+popd
