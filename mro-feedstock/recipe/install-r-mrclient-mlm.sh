babf28a78d6dc702cbf54e6d4ba94bb50eb7d5a3
Ray Donnelly <mingw.android@gmail.com>
2018-02-24 19:00:30 +0000

    mro: Fixes for MLM

--- mro-feedstock/recipe/install-r-mrclient-mlm.sh
+++ mro-feedstock/recipe/install-r-mrclient-mlm.sh
@@ -7,7 +7,16 @@ PREFIX_LIB="$PREFIX"/$LIBRARY
 
 mkdir -p "$PREFIX_LIB"/MicrosoftML/mxLibs/x64/AutoLoad
 pushd unpack/mlm-training-models/opt/microsoft/rclient/$PKG_VERSION/libraries/RServer/MicrosoftML/mxLibs/x64/AutoLoad || exit 1
-  mv *.model "$PREFIX_LIB"/MicrosoftML/mxLibs/x64/AutoLoad/ || exit 1
+  for MODEL in $(find . -name "*.model" | cut -c 3-); do
+    mv $MODEL "$PREFIX_LIB"/MicrosoftML/mxLibs/x64/$MODEL
+    # Let us see how well symlinks work on Windows. We do not need the symlink to be a
+    # symlink or to even exist we just need conda not to fall over on them at any point.
+    # On Linux they need to work properly though since MicrosoftML looks in AutoLoad for
+    # some reason.
+    pushd "$PREFIX_LIB"/MicrosoftML/mxLibs/x64/AutoLoad
+      ln -s ../$MODEL $MODEL
+    popd
+  done
 popd
 
 find . | wc -l
