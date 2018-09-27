bdfbacb44d86fde8b46d3dc6b8284ec980cf50c9
Ray Donnelly <mingw.android@gmail.com>
2018-09-27 12:05:18 -0500

    Windows fixes for MRO 3.5.1

--- r-rcurl-feedstock/recipe/bld.bat
+++ r-rcurl-feedstock/recipe/bld.bat
@@ -1,2 +1,6 @@
+copy %SRC_DIR%\libcurl\lib\x64\libcurl-dualssl.a %SRC_DIR%\libcurl\lib\x64\libcurl.a
+copy %SRC_DIR%\libcurl\lib\i386\libcurl-dualssl.a %SRC_DIR%\libcurl\lib\i386\libcurl.a
+
+set LIB_CURL=%SRC_DIR:\=/%/libcurl
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
